3e78b5b0e6a3b9fddfe3c59a91b743cfee780aaf
D1slike <lilipp1q@yandex.ru>
2019-02-17 13:24:33 +0300

    lab 2

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,31 @@
 var mongoose = require("mongoose"),
-  Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
 
 var UserSchema = mongoose.Schema({
-  login: String,
-  password: String,
-  first_name: String,
-  last_name: String
+    login: String,
+    password: String,
+    first_name: String,
+    last_name: String
 });
+
+// https://www.npmjs.com/package/jsonwebtoken документация + примеры по библиотеке jwt
+
+UserSchema.statics.checkAuth = () => (req, res, next) => {
+    // получение токена из присланных кук, req.cookies[AUTH_COOKIE_NAME]
+    // проверка токена jwt.verify (возвращает объект, который положили в getJwtToken)
+    // если токен проешел проверку то
+    // 1)в объект req положить объект с id пользователя req.user = {}
+    // 2)вызываем функцию next()
+    // иначе возвращаем 401 ошибку
+};
+
+UserSchema.methods.getJwtToken = function () {
+    const {_id, login} = this;
+
+    // сгененрировать токен, используя функцию jwt.sign,
+    // секрет лежит здесь process.env.JWT_TOKEN_SECRET
+};
 
 var User = mongoose.model("User", UserSchema);
 module.exports = User;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const { createHash } = require('../utils');
+const {createHash} = require('../utils');
 
 const User = require("../models/User");
 
@@ -15,12 +15,12 @@ router.post("/login", async (req, res) => {
     //Ответ клиету отдаётся в 2-ух форматах
     // 1. res.json({status: "ok", user});
     // 2. res.json({status: "error", error: "Неверный пароль"});
-    
+
     //Метод createHash создаёт хэш для безопасного хранения пароля в бд
 });
 
 router.post("/", async (req, res) => {
-    console.log('req.body registration', req.body , req.cookies);
+    console.log('req.body registration', req.body, req.cookies);
     //Переданные параметры запроса хранятся в req.body
 
     // Для создания пользователя необходимо вызвать
@@ -32,6 +32,24 @@ router.post("/", async (req, res) => {
     // 2. res.json({status: "error", error: "Неверный пароль"});
 
     //Метод createHash создаёт хэш для безопасного хранения пароля в бд
+});
+
+router.use("/me", User.checkAuth()); //добавление middleware, для проверки токена при каждом запросе
+router.get("/me", async (req, res) => { //метод возвраащает профиль пользователя
+    console.log("req.user me", req.user);
+
+    const userId = req.user._id;
+    if (!userId) {
+        res.json({status: "error", error: "Индетификатор пользователя не найден"});
+        return;
+    }
+
+    try {
+        const user = await User.findOne({_id: userId});
+        res.json({status: "ok", user});
+    } catch (err) {
+        console.error(err)
+    }
 });
 
 module.exports = router;
